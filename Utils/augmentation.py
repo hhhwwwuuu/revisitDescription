b23e87477c7aa8ac202a13ea2902106537bfb956
wzq <wzq0515@gmail.com>
2020-12-30 10:49:02 +0900

    dataaugment

--- Utils/augmentation.py
+++ Utils/augmentation.py
@@ -9,5 +9,24 @@ This library is used for data augmentation.
 from BackTranslation import BackTranslation
 
 
+class DataAugment():
+    def __init__(self, url = 'https://translate.google.com/'):
+        self.url == url
+        self.trans = BackTranslation()
+
+
+    def translate(self, data, src='en', tmp='zh-cn'):
+        '''
+            only back-translate the positive smaples
+            :param data: DataFrame. --> labelled dataset
+            :return: back-translated data
+            '''
+        # TODO: translate the sentence to generate more positive samples if the sample is labeled on any permission.
+
+
+
+
+
+
 
 
